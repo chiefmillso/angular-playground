fb3bedf01c618587cc9c0a4be90039c5b6a117fa
David Miller <david@davidmiller.co>
2015-04-02 22:13:37 +1000

    naive implementation of watched films

--- quickstart/src/components/home/home.ctrl.js
+++ quickstart/src/components/home/home.ctrl.js
@@ -1,8 +1,41 @@
-angular.module('myApp.home', [])
-  .controller('HomeController', function() {
-    this.name = 'Pascal';
+angular.module('myApp.home', ['myApp.common.services'])
+  .controller('HomeController', ['films.svc', function(filmsSvc) {
+    var _this = this;
+    this.suggestion = null;
+    this.rawData = [];
 
-    this.changeName = function() {
-      this.name = 'Christoph';
-    }
-  });
+    this.filter = function(d) {
+      var film = _.chain(d).filter(function(item) {
+          return item.watched === false;
+        })
+        .sortBy(function() {
+          return _.random(0, d.length);
+        })
+        .first(1)
+        .value();
+      if (film.length === 1) {
+        _this.suggestion = film[0];
+      } else {
+        _this.suggestion = null;
+      }
+    };
+
+    this.markSeenIt = function() {
+      _this.suggestion.watched = true;
+      _this.filter(_this.rawData);
+    };
+
+    this.markNotInterested = function() {
+      _this.filter(_this.rawData);
+    };
+
+    this.markJustWatchedIt = function() {
+      _this.suggestion.watched = true;
+      _this.filter(_this.rawData);
+    };
+
+    filmsSvc.then(function(data) {
+      _this.rawData = data;
+      _this.filter(data);
+    });
+  }]);
